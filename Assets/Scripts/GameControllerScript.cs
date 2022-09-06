a797da4a91538d4dc742ae2958aa76b429a3a5d0
Detravoir <thomasdiondehaas@hotmail.com>
2022-09-06 15:16:30 +0200

    full game

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameControllerScript : MonoBehaviour
 {
     bool cheatSheetIsOpen = false;
     bool CMDIsOpen = false;
+    bool HelloWorldIsOpen = false;
     public GameObject cheatsheet;
     public GameObject cmd;
+    public GameObject helloworld;
 
     void Start()
     {
-
+        
     }
+
 
     public void Update()
     {   
         CheatSheetOpen();
         CMDOpen();
+        HelloWorldOpen();
     }
 
     public void CheatSheetOpen()
@@ -117,6 +123,56 @@ public class GameControllerScript : MonoBehaviour
         else
         {
             cmd.SetActive(true);
+        }
+    }
+
+    public void HelloWorldOpen()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Check if collides with tag: cheatsheetfile
+            if (hit.collider.tag == "helloworldicon")
+            {
+                if (HelloWorldIsOpen == false)
+                {
+                    HelloWorldIsOpen = true;
+                    //Debug.Log("Cheatsheet is visable");
+                }
+                else
+                {
+                    HelloWorldIsOpen = false;
+                    //Debug.Log("Cheatsheet is NOT visable");
+                }
+                Debug.Log(HelloWorldIsOpen);
+            }
+
+            //Check if collides with tag: cheatsheetx
+            if (hit.collider.tag == "helloworldx")
+            {
+                if (HelloWorldIsOpen == false)
+                {
+                    HelloWorldIsOpen = true;
+                }
+                else
+                {
+                    HelloWorldIsOpen = false;
+                }
+                Debug.Log(HelloWorldIsOpen);
+            }
+        }
+
+        if (HelloWorldIsOpen == false)
+        {
+            helloworld.SetActive(false);
+        }
+        else
+        {
+            helloworld.SetActive(true);
         }
     }
 }
